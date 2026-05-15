c41f46b794aec0daaceab8caa7b14c482ff92d3c
togiwan <tolqaoner@gmail.com>
2026-05-15 19:46:39 +0300

    Remove browser transfer mode

--- Sources/AndroidBridge/TransferMode.swift
+++ Sources/AndroidBridge/TransferMode.swift
@@ -15,19 +15,3 @@ enum TransferMode: String, CaseIterable, Identifiable {
         }
     }
 }
-
-enum WirelessTransferMode: String, CaseIterable, Identifiable {
-    case browser
-    case adbWireless
-
-    var id: String { rawValue }
-
-    var title: String {
-        switch self {
-        case .browser:
-            "Browser Transfer"
-        case .adbWireless:
-            "ADB Wireless"
-        }
-    }
-}
--- Sources/AndroidBridge/WirelessTransferView.swift
+++ Sources/AndroidBridge/WirelessTransferView.swift
@@ -5,23 +5,6 @@ struct WirelessTransferView: View {
     @State private var store = WirelessTransferStore()
 
     var body: some View {
-        VStack(spacing: 0) {
-            Picker("Wireless Mode", selection: $store.selectedMode) {
-                ForEach(WirelessTransferMode.allCases) { mode in
-                    Text(mode.title).tag(mode)
-                }
-            }
-            .pickerStyle(.segmented)
-            .padding(14)
-
-            Divider()
-
-            switch store.selectedMode {
-            case .browser:
-                BrowserTransferView(store: store)
-            case .adbWireless:
-                ADBWirelessView(store: store, androidStore: androidStore)
-            }
-        }
+        ADBWirelessView(store: store, androidStore: androidStore)
     }
 }
